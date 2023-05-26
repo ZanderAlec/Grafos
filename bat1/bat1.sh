f15b59d0d3fcd3e92bfa5fe77cf5e87ebdd7f8cd
Iasmin Borba <icb2@ic.ufal.br>
2023-05-25 23:50:58 -0300

    Update bat1.sh

--- bat1/bat1.sh
+++ bat1/bat1.sh
@@ -1,16 +1,18 @@
+bellman=./bellman-ford.bin
+
 echo "Testando algoritmo de Bellman-Ford"
 for i in instances/*.txt
 do
 	echo -e "\e[33mInstância $i\e[0m";
-	$dijkstra -f $i > temp;
+	$bellman -f $i > temp;
 
 	j=$(basename $i);
 	diff -w temp ./sp/$j > /dev/null ;
-	rm temp;
 	if [ $? -eq 0 ]; then
 		echo -e "\e[32mOK\e[0m"
 	else
 		echo -e "\e[31mErro\e[0m";
 	fi
+	rm temp;
 
 done
